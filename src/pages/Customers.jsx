407b5ea4e198c5a7320cb24ee92d547e94d80364
sammykirigha <dkirigha18@gmail.com>
2022-05-30 09:06:58 +0300

    added new customers data and table

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -5,28 +5,35 @@ import {
     ColumnsDirective,
     ColumnDirective,
     Page,
-    Search,
-    Inject,
+    Selection,
+    Sort,
+    Filter,
+  Inject,
+    Edit
 } from "@syncfusion/ej2-react-grids";
 import Header from "../components/Header";
-import { employeesData, employeesGrid } from "../data/dummy";
+import { customersData, customersGrid } from "../data/dummy";
 
 
 
 const Customers = () => {
   return <div className="m-2 md:m-10 p-2 md:p10 bg-white rounded-3xl">
-    <Header category="Page" title="Employees" />
+    <Header category="Page" title="Customers" />
     <GridComponent
       id="gridcomp"
-      dataSource={employeesData}
+      dataSource={customersData}
       allowPaging
+      allowSorting
+      toolbar={['Delete']}
+      editSettings={{ allowDeleting: true, allowEditing: true }}
+      width="auto"
     >
       <ColumnsDirective>
-        {employeesGrid.map((item, index) => (
+        {customersGrid.map((item, index) => (
           <ColumnDirective key={index} {...item} />
         ))}
       </ColumnsDirective>
-      <Inject services={[ Page, Search]}  />
+      <Inject services={[ Page, Selection, Edit, Sort, Filter]}  />
     </GridComponent>
     </div>;
 };
